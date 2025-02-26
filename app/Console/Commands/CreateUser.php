45cf061e4fc179f3322184f3cc6ba3450c89cdd5
Diego Gomez <dgomez@sisconlatam.com>
2025-02-26 12:39:45 -0600

    Add: commands to admin user tokens

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password as RulesPassword;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-user {name} {email} {password} {password_confirmation}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $inputs = $this->arguments();
+
+        try {
+            Validator::make($inputs, [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => [
+                    'required',
+                    'string',
+                    'email',
+                    'max:255',
+                    Rule::unique(User::class),
+                ],
+                'password' => ['required', 'string', RulesPassword::default(), 'confirmed'],
+            ])->validate();
+
+            User::create([
+                'name' => $inputs['names'],
+                'email' => $inputs['email'],
+                'password' => Hash::make($inputs['password']),
+            ]);
+
+            $this->info('User registered.');
+        } catch (\Throwable $th) {
+            $this->error($th);
+        }
+    }
+}
